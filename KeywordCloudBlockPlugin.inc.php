a060b8bba9f5ab72ae148e23c9d14c7f1102103e
ada <ada@lepidus.com.br>
2020-05-11 18:05:53 -0400

    Modifica classe principal para funcionar na nova versão
    Modificações para que o plugin apareça na lista de plugin,
    Modificações nas classes de Publication para Submit.
    
    Signed-off-by: Ada <ada@lepidus.com.br>
    Signed-off-by: Matheus <matheus@lepidus.com.br>

--- KeywordCloudBlockPlugin.inc.php
+++ KeywordCloudBlockPlugin.inc.php
@@ -1,3 +1,4 @@
+ 
 <?php
 
 /**
@@ -17,7 +18,9 @@ define('KEYWORD_BLOCK_MAX_ITEMS', 100);
 define('KEYWORD_BLOCK_CACHE_DAYS', 2);
 
 import('lib.pkp.classes.plugins.BlockPlugin');
-import('classes.article.ArticleDAO');
+import ('classes.submission.SubmissionDAO');
+
+
 
 class KeywordCloudBlockPlugin extends BlockPlugin {
 	/**
@@ -65,8 +68,8 @@ class KeywordCloudBlockPlugin extends BlockPlugin {
 		$keywords =& $cache->getContents();
 		if (empty($keywords)) return '';
 		
-		$templateMgr->addJavaScript('d3','https://d3js.org/d3.v4.min.js');
-		$templateMgr->addJavaScript('d3.layout.cloud','https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.2.0/d3.layout.cloud.min.js');
+		$templateMgr->addJavaScript('d3','https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js');
+		$templateMgr->addJavaScript('d3.layout.cloud','https://cdnjs.cloudflare.com/ajax/libs/d3-cloud/1.0.0/d3.layout.cloud.min.js');
 		$templateMgr->addJavaScript('d3.wordcloud',$this->getJavaScriptURL($request).'d3.wordcloud.min.js');
 
 
@@ -77,16 +80,26 @@ class KeywordCloudBlockPlugin extends BlockPlugin {
 	function _cacheMiss($cache, $id) {
 
 		//Get all published Articles of this Journal
-		$publishedArticleDao = DAORegistry::getDAO('PublishedArticleDAO');
-		$publishedArticles =& $publishedArticleDao->getPublishedArticlesByJournalId($cache->getCacheId(), $rangeInfo = null, $reverse = true);
+		$SubmissionDAO = DAORegistry::getDAO('SubmissionDAO');
+		
 		//Get all IDs of the published Articles
+		import('classes.submission.Submission');
+		$submissionsIterator = Services::get('submission')->getMany([
+			'cache' => $cache->getCacheId(),
+			'status' => STATUS_PUBLISHED,
+		]);
+		error_log('ID do cache:');
+		
 		$submissionKeywordDao = DAORegistry::getDAO('SubmissionKeywordDAO');
+		error_log($cache->getCacheId(), 0);
+
 		//Get all Keywords from all published articles of this journal
 		$all_keywords = array();
-		while ($publishedArticle = $publishedArticles->next()) {
-			$article_keywords = $submissionKeywordDao->getKeywords($publishedArticle->getId(),
-				array(AppLocale::getLocale()))[AppLocale::getLocale()];
-			$all_keywords = array_merge($all_keywords, $article_keywords);
+		foreach ($submissionsIterator as $submission) {
+			$articleId = $submission->getId();
+			error_log($articleId, 0);
+			$submission_keywords = $submissionKeywordDao->getKeywords($articleId, array(AppLocale::getLocale()));
+			$all_keywords = array_merge($all_keywords, $submission_keywords);
 		}
 
 		//Count the keywords					
@@ -100,7 +113,7 @@ class KeywordCloudBlockPlugin extends BlockPlugin {
 		$top_keywords = array_slice($count_keywords, 0, KEYWORD_BLOCK_MAX_ITEMS);
 		
 		$keywords = array();
-		
+
 
 		foreach ($top_keywords as $k => $c) {
 			$kw = new stdClass();
