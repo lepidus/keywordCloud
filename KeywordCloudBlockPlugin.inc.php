dbf4c896ddf22d7ff842095f79bb22e64a79c02f
yves <yves@lepidus.com.br>
2023-04-04 14:33:41 -0400

    Rename $journal variable to $context.

--- KeywordCloudBlockPlugin.inc.php
+++ KeywordCloudBlockPlugin.inc.php
@@ -17,117 +17,126 @@
 define('KEYWORD_BLOCK_MAX_ITEMS', 50);
 define('KEYWORD_BLOCK_CACHE_DAYS', 2);
 define('ONE_DAY_SECONDS', 60 * 60 * 24);
-define('TWO_DAYS_SECONDS',ONE_DAY_SECONDS * KEYWORD_BLOCK_CACHE_DAYS);
+define('TWO_DAYS_SECONDS', ONE_DAY_SECONDS * KEYWORD_BLOCK_CACHE_DAYS);
 
 import('lib.pkp.classes.plugins.BlockPlugin');
-import ('classes.submission.SubmissionDAO');
+import('classes.submission.SubmissionDAO');
 
-class KeywordCloudBlockPlugin extends BlockPlugin {
-	/**
-	 * Install default settings on journal creation.
-	 * @return string
-	 */
-	function getContextSpecificPluginSettingsFile() {
-		return $this->getPluginPath() . '/settings.xml';
-	}
+class KeywordCloudBlockPlugin extends BlockPlugin
+{
+    /**
+     * Install default settings on journal creation.
+     * @return string
+     */
+    public function getContextSpecificPluginSettingsFile()
+    {
+        return $this->getPluginPath() . '/settings.xml';
+    }
 
-	/**
-	 * Get the display name of this plugin.
-	 * @return String
-	 */
-	function getDisplayName() {
-		return __('plugins.block.keywordCloud.displayName');
-	}
+    /**
+     * Get the display name of this plugin.
+     * @return String
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.block.keywordCloud.displayName');
+    }
 
-	/**
-	 * fallBack for getFileCache
-	 */
-	function cacheDismiss() {
-		return null;
-	}
-	/**
-	 * Get a description of the plugin.
-	 */
-	function getDescription() {
-		return __('plugins.block.keywordCloud.description');
-	}
+    /**
+     * fallBack for getFileCache
+     */
+    public function cacheDismiss()
+    {
+        return null;
+    }
+    /**
+     * Get a description of the plugin.
+     */
+    public function getDescription()
+    {
+        return __('plugins.block.keywordCloud.description');
+    }
 
-	/**
-	 * @see BlockPlugin::getContents
-	 */
-	function getContents($templateMgr, $request = null) {
-		$journal = $request->getContext();
-		if (!$journal) return '';
-		
-		$locale = AppLocale::getLocale();
-		$cacheManager = CacheManager::getManager();
-		$cache = $cacheManager->getFileCache(
-			$journal->getId(),
-			'keywords_'. $locale,
-			[$this, 'cacheDismiss']
-		);
+    /**
+     * @see BlockPlugin::getContents
+     */
+    public function getContents($templateMgr, $request = null)
+    {
+        $context = $request->getContext();
+        if (!$context) {
+            return '';
+        }
 
-		$keywords =& $cache->getContents();
-		$currentCacheTime = time() - $cache->getCacheTime();
-		
-		if ($currentCacheTime > TWO_DAYS_SECONDS){
-			$cache->flush();
-			$cache->setEntireCache($this->getKeywordsJournal($journal->getId()));
-		}
-		else if ($keywords == "[]"){
-			$cache->setEntireCache($this->getKeywordsJournal($journal->getId()));
-		}
+        $locale = AppLocale::getLocale();
+        $cacheManager = CacheManager::getManager();
+        $cache = $cacheManager->getFileCache(
+            $context->getId(),
+            'keywords_'. $locale,
+            [$this, 'cacheDismiss']
+        );
 
-		$templateMgr->addJavaScript('d3','https://d3js.org/d3.v4.js');
-		$templateMgr->addJavaScript('d3-cloud','https://cdn.jsdelivr.net/gh/holtzy/D3-graph-gallery@master/LIB/d3.layout.cloud.js');
+        $keywords =& $cache->getContents();
+        $currentCacheTime = time() - $cache->getCacheTime();
 
-		$templateMgr->assign('keywords', $keywords);
-		return parent::getContents($templateMgr, $request);
-	}
-	
-	function getKeywordsJournal($journalId){
-		$submissionKeywordDao = DAORegistry::getDAO('SubmissionKeywordDAO');
+        if ($currentCacheTime > TWO_DAYS_SECONDS) {
+            $cache->flush();
+            $cache->setEntireCache($this->getKeywordsJournal($context->getId()));
+        } elseif ($keywords == "[]") {
+            $cache->setEntireCache($this->getKeywordsJournal($context->getId()));
+        }
 
-		//Get all IDs of the published Articles
-		$submissionsIterator = Services::get('submission')->getMany([
-			'contextId' => $journalId,
-			'status' => STATUS_PUBLISHED,
-		]);
-		
-		//Get all Keywords from all published articles of this journal
-		$all_keywords = array();
-		$currentLocale = AppLocale::getLocale();
-		foreach ($submissionsIterator as $submission) {
-			$publications = $submission->getPublishedPublications();
+        $templateMgr->addJavaScript('d3', 'https://d3js.org/d3.v4.js');
+        $templateMgr->addJavaScript('d3-cloud', 'https://cdn.jsdelivr.net/gh/holtzy/D3-graph-gallery@master/LIB/d3.layout.cloud.js');
 
-			foreach ($publications as $publication){
-				$publi_keywords = $submissionKeywordDao->getKeywords($publication->getId(), array($currentLocale));
-				
-				if(count($publi_keywords) > 0) {
-					$all_keywords = array_merge($all_keywords, $publi_keywords[$currentLocale]);
-				}
-			}
-		}
-		//Count the keywords and sort them in a frequency basis
-		$count_keywords = array_count_values($all_keywords);
-		arsort($count_keywords, SORT_NUMERIC);
+        $templateMgr->assign('keywords', $keywords);
+        return parent::getContents($templateMgr, $request);
+    }
 
-		// Put only the most often used keywords in an array
-		// maximum of KEYWORD_BLOCK_MAX_ITEMS
-		$top_keywords = array_slice($count_keywords, 0, KEYWORD_BLOCK_MAX_ITEMS);
-		$keywords = array();
+    public function getKeywordsJournal($journalId)
+    {
+        $submissionKeywordDao = DAORegistry::getDAO('SubmissionKeywordDAO');
 
-		foreach ($top_keywords as $key => $countKey) {
-			$keyWords = new stdClass();
-			$keyWords->text = $key;
-			$keyWords->size = $countKey;
-			$keywords[] = $keyWords;
-		}
-		
-		return json_encode($keywords);
-	}
-	
-	function getJavaScriptURL($request) {
-		return $request->getBaseUrl() . '/' . $this->getPluginPath() . '/js/';
-	}
+        //Get all IDs of the published Articles
+        $submissionsIterator = Services::get('submission')->getMany([
+            'contextId' => $journalId,
+            'status' => STATUS_PUBLISHED,
+        ]);
+
+        //Get all Keywords from all published articles of this journal
+        $all_keywords = array();
+        $currentLocale = AppLocale::getLocale();
+        foreach ($submissionsIterator as $submission) {
+            $publications = $submission->getPublishedPublications();
+
+            foreach ($publications as $publication) {
+                $publi_keywords = $submissionKeywordDao->getKeywords($publication->getId(), array($currentLocale));
+
+                if(count($publi_keywords) > 0) {
+                    $all_keywords = array_merge($all_keywords, $publi_keywords[$currentLocale]);
+                }
+            }
+        }
+        //Count the keywords and sort them in a frequency basis
+        $count_keywords = array_count_values($all_keywords);
+        arsort($count_keywords, SORT_NUMERIC);
+
+        // Put only the most often used keywords in an array
+        // maximum of KEYWORD_BLOCK_MAX_ITEMS
+        $top_keywords = array_slice($count_keywords, 0, KEYWORD_BLOCK_MAX_ITEMS);
+        $keywords = array();
+
+        foreach ($top_keywords as $key => $countKey) {
+            $keyWords = new stdClass();
+            $keyWords->text = $key;
+            $keyWords->size = $countKey;
+            $keywords[] = $keyWords;
+        }
+
+        return json_encode($keywords);
+    }
+
+    public function getJavaScriptURL($request)
+    {
+        return $request->getBaseUrl() . '/' . $this->getPluginPath() . '/js/';
+    }
 }
