d1b147ec473b431a3880fdf8713874cd0cfff412
Jhon <jhon@lepidus.com.br>
2023-07-18 17:12:22 -0400

    Adds integration tests to be executed automatically
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ cypress/tests/Test1_enableAndAddPluginToSidebar.spec.js
@@ -0,0 +1,27 @@
+describe('Keyword Cloud - Enable plugin and add it to the sidebar', function() {
+    it('Enable Plugin', function() {
+        cy.login('dbarnes', null, 'publicknowledge');
+
+        cy.get('a:contains("Website")').click();
+
+		cy.waitJQuery();
+		cy.get('#plugins-button').click();        
+        cy.get('input[id^=select-cell-keywordcloudblockplugin]').check();
+		cy.get('input[id^=select-cell-keywordcloudblockplugin]').should('be.checked');
+    });
+    it('Add plugin to sidebar', function() {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.get('a:contains("Website")').click();
+        
+        cy.waitJQuery();
+		cy.get('#appearance-button').click();
+        cy.get('#appearance-setup-button').click();
+        cy.get('input[value="keywordcloudblockplugin"]').check();
+        cy.get('button:contains("Save"):visible').click();
+    });
+    it('Check presence of keyword cloud block in public site', function() {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.contains('Journal of Public Knowledge').click();
+        cy.get('.block_Keywordcloud');
+    });
+});
